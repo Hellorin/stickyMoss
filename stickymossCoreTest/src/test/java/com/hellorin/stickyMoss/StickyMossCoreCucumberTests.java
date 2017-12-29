e6d5a52db323fc72daaf865724a35068ef3221d2
Hellorin <davi.lawrence@gmail.com>
2017-12-29 14:12:42 +0100

    Added module for acceptance tests of core backend

---
+++ stickymossCoreTest/src/test/java/com/hellorin/stickyMoss/StickyMossCoreCucumberTests.java
@@ -0,0 +1,14 @@
+package com.hellorin.stickyMoss;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.junit.Cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+@RunWith(Cucumber.class)
+public class StickyMossCoreCucumberTests {
+
+}
